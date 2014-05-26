91148cc33c39ff2640de4a90c6d8eeef18e724e3
Rooth <roothb@gmail.com>
2014-05-26 14:07:36 -0400

    May26Carousel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'amenities/index'
+
+  get 'info/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
